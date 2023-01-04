b4ef7b243f0744bdcca923a7ca2b24f89a87a9b8
Ronald Alfonso <raalfonsoparra@gmail.com>
2023-01-04 17:55:46 -0400

    navbar

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  currentSection: BehaviorSubject<String> = new BehaviorSubject<String>('menu')
+  sections: string[] = ['menu', 'content']
+
+  constructor() {
+    document.addEventListener('scroll', () => {
+      console.log('hola');
+      this.addSticky();
+    })
+  }
+
+  addSticky() {
+      const menu = document.getElementById('menu');
+      const content = document.getElementById('content');
+      if (menu != null && content != null) {
+        const rectMenu = menu.getBoundingClientRect();
+        const rectContent = content.getBoundingClientRect();
+          if (rectMenu.top <= 0 && rectContent.top <= -115) {
+            menu.classList.add('sticky')
+          } else if (rectContent.top > -200 && rectContent.top < 0) {
+            menu.classList.remove('sticky')
+          }
+
+      }
+  }
 
 }
